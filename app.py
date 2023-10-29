f1e8688a91b86d47b4046e2453c9b35d113d4f90
itsDev-Developer <148090753+itsDev-Developer@users.noreply.github.com>
2023-10-29 10:53:19 +0530

    Update app.py
    dg

--- app.py
+++ app.py
@@ -15,6 +15,7 @@
 
 import os, tornado
 from telegram import Bot
+from acutebot import TOKEN
 from acutebot.helpers.database.spotify_sql import update_creds
 from acutebot.helpers.spthelper import SpotifyClient
 
